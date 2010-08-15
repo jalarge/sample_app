f6f8f02696131889f78a10b0f7dc7298353b999f
jalarge <jalarge@gmail.com>
2010-08-15 07:56:08 -0700

    Done with user edit/update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,6 +12,10 @@ module SessionsHelper
   def current_user
     @current_user ||= user_from_remember_token
   end
+  
+  def current_user?(user)
+    user == current_user
+  end
 
   def signed_in?
     !current_user.nil?
@@ -23,6 +27,24 @@ module SessionsHelper
     self.current_user = nil
   end
 
+  def deny_access
+    store_location
+    flash[:notice] = "Please sign in to access this page."
+    redirect_to signin_path
+  end
+
+  def store_location
+    session[:return_to] = request.fullpath
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    clear_return_to
+  end
+
+  def clear_return_to
+    session[:return_to] = nil
+  end
 
   private 
   #----------------------------------------------------------
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100810195606) do
+ActiveRecord::Schema.define(:version => 20100814151550) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(:version => 20100810195606) do
     t.datetime "updated_at"
     t.string   "encrypted_password"
     t.string   "salt"
+    t.boolean  "admin",              :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
